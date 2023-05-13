1abff70421e3c84cb24a4bfeaa8287e945c9906b
BrandonKirbyson <brandonkirbyson@gmail.com>
2023-05-13 09:07:42 -0700

    CSS properly loads

---
+++ src/css.ts
@@ -0,0 +1,25 @@
+import * as vscode from "vscode";
+
+/**
+ * Gets the combined CSS content from the css files
+ * @param cssFiles The css files to combine
+ * @param cssRoot The root directory of the css files
+ * @returns The combined CSS content as a string
+ */
+export async function getUpdatedCSS(
+  cssFiles: `${string}.css`[],
+  cssRoot: string
+): Promise<string> {
+  let css = "";
+  for (const cssFile of cssFiles) {
+    try {
+      //Reads the file and adds the content to the css string
+      await vscode.workspace.fs
+        .readFile(vscode.Uri.file(cssRoot + cssFile))
+        .then((data) => (css += data.toString()));
+    } catch (error) {
+      console.error("Error reading css file", error);
+    }
+  }
+  return css;
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,6 @@
 import * as vscode from "vscode";
 import loadAnimations from "./animations";
+import { getUpdatedCSS } from "./css";
 
 export function activate(context: vscode.ExtensionContext) {
   const jsonPath = context.extensionPath + "/animations.json";
@@ -16,4 +17,37 @@ export function activate(context: vscode.ExtensionContext) {
   vscode.workspace.fs.readFile(vscode.Uri.file(jsonPath)).then((data) => {
     loadAnimations(JSON.parse(data.toString()));
   });
+
+  const cssDirectory = context.extensionPath + "/dist/css/";
+  const rootCSSPath = context.extensionPath + "/style.css";
+
+  getUpdatedCSS(["Default-Transitions.css", "test.css"], cssDirectory).then(
+    (css) => {
+      vscode.workspace.fs.writeFile(
+        vscode.Uri.file(rootCSSPath),
+        Buffer.from(css)
+      );
+    }
+  );
+
+  addToConfig(rootCSSPath);
+}
+
+/**
+ * Adds the CSS filepath to the list of imports in the settings.json file for the Custom CSS and JS Loader extension
+ * @param rootCSSPath The path to the root CSS file
+ */
+function addToConfig(rootCSSPath: string) {
+  const config = vscode.workspace.getConfiguration();
+  const customCssImports = config.get<string[]>("vscode_custom_css.imports"); //Get the current list of imports
+  if (customCssImports && !customCssImports.includes(rootCSSPath)) {
+    //If the list exists and the root CSS file is not already in the list
+    customCssImports.push(rootCSSPath); //Add the root CSS file to the list
+    config.update(
+      //Update the list of imports
+      "vscode_custom_css.imports",
+      customCssImports,
+      vscode.ConfigurationTarget.Global
+    );
+  }
 }
