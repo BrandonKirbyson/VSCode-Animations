71de5ad14490d7a17acd9c0653514419599a1f5b
BrandonKirbyson <brandonkirbyson@gmail.com>
2023-05-14 09:10:59 -0700

    Added documentation

--- src/extension.ts
+++ src/extension.ts
@@ -5,8 +5,11 @@ import { getUpdatedCSS } from "./css";
 
 export function activate(context: vscode.ExtensionContext) {
   const jsonPath = context.extensionPath + "/animations.json";
-  const rootCSSPath = "file://" + context.extensionPath + "/style.css";
+  const rootCSSPath = "file://" + context.extensionPath + "/style.css"; //The path to the root css file which should be used for Custom CSS extension
 
+  /**
+   * Register the command to open the settings file
+   */
   context.subscriptions.push(
     vscode.commands.registerCommand(
       "VSCode-Animations.openAnimationSettings",
@@ -16,36 +19,50 @@ export function activate(context: vscode.ExtensionContext) {
     )
   );
 
+  /**
+   * Register the command to disable the root css file in the Custom CSS extension
+   */
   context.subscriptions.push(
     vscode.commands.registerCommand(
       "VSCode-Animations.disableAnimations",
       () => {
         vscode.window.showInformationMessage("Disabled Animations");
+        //Remove the root css file from the Custom CSS extension
         removeFromConfig(rootCSSPath).then(() => {
-          vscode.commands.executeCommand("extension.updateCustomCSS");
+          vscode.commands.executeCommand("extension.updateCustomCSS"); //Reload the Custom CSS extension
         });
       }
     )
   );
 
+  /**
+   * Register the command to enable the root css file in the Custom CSS extension and reload the Custom CSS extension
+   */
   context.subscriptions.push(
     vscode.commands.registerCommand(
       "VSCode-Animations.enableAnimations",
       () => {
         vscode.window.showInformationMessage("Enabled Animations");
+        //Add the root css file to the Custom CSS extension
         addToConfig(rootCSSPath).then(() => {
-          vscode.commands.executeCommand("extension.installCustomCSS");
+          vscode.commands.executeCommand("extension.installCustomCSS"); //Reload the Custom CSS extension
         });
       }
     )
   );
 
+  /**
+   * Loads the animations from the animations.json file
+   */
   vscode.workspace.fs.readFile(vscode.Uri.file(jsonPath)).then((data) => {
     loadAnimations(JSON.parse(data.toString()));
   });
 
-  const cssDirectory = context.extensionPath + "/dist/css/";
+  const cssDirectory = context.extensionPath + "/dist/css/"; //The path to the css directory
 
+  /**
+   * Get the updated css and write it to the root css file
+   */
   getUpdatedCSS(
     [
       "Default-Transitions.css",
