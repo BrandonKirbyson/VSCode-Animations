9d36f84983a891aa7dead7afbc7f21de3397d04e
BrandonKirbyson <brandonkirbyson@gmail.com>
2023-05-22 18:40:47 -0700

    Tab animation now works properly

--- src/updateHandler.ts
+++ src/updateHandler.ts
@@ -1,8 +1,3 @@
-declare const document: any; //Declare document for ts to not complain (actual document is not available in this context)
-
-// eslint-disable-next-line @typescript-eslint/naming-convention
-declare const MutationObserver: any; //Declare MutationObserver for ts to not complain (actual MutationObserver is not available in this context)
-
 console.log("UpdateHandler: Enabled");
 
 /**
@@ -40,6 +35,8 @@ console.log("UpdateHandler: Enabled");
     });
   }, 10);
 
+  addAnimationUtil();
+
   /**
    * Creates the custom css element and inserts it into the document
    */
@@ -49,7 +46,7 @@ console.log("UpdateHandler: Enabled");
 
     styleElement.textContent = css;
 
-    document.body.insertAdjacentElement("afterBegin", styleElement);
+    document.body.insertAdjacentElement("afterbegin", styleElement);
   }
 
   /**
@@ -68,5 +65,106 @@ console.log("UpdateHandler: Enabled");
     if (styleElement.textContent === css) return; //If the css is the same, return
 
     styleElement.textContent = css;
+  }
+
+  function addAnimationUtil() {
+    /*
+    <div class="tabs-container">
+      <div class="tab"></div>
+      <div class="tab"></div>
+      <div class="tab"></div>
+    </div>
+    */
+
+    //Add a mutation observer to the tabs-container to check for when a tab is added or removed
+    const observer = new MutationObserver((mutations) => {
+      const mutationData: {
+        added: null | Node;
+        removed: null | Node;
+        updated: Node[];
+      } = {
+        added: null,
+        removed: null,
+        updated: [],
+      };
+      mutations.forEach((mutation) => {
+        //Check that the target is only .tabs-container or a direct child of it
+        if (
+          (mutation.target as HTMLElement).className !== "tabs-container" &&
+          mutation.target.parentElement?.className !== "tabs-container"
+        )
+          return;
+
+        if (mutation.type === "childList") {
+          if (mutation.addedNodes.length > 0) {
+            mutationData.added = mutation.addedNodes[0]; //If a tab was added
+          }
+
+          if (mutation.removedNodes.length > 0) {
+            mutationData.removed = mutation.removedNodes[0]; //If a tab was removed
+          }
+
+          // Get the first tab that just had its attributes changed
+        } else if (mutation.type === "attributes") {
+          if (mutation.attributeName !== "title") return;
+
+          //Check if the title actually changed
+          if (
+            mutation.oldValue ===
+            (mutation.target as HTMLElement).getAttribute("title")
+          )
+            return;
+
+          mutationData.updated.push(mutation.target);
+        }
+      });
+
+      if (
+        mutationData.updated.length === 0 &&
+        !mutationData.added &&
+        !mutationData.removed
+      )
+        return; //If nothing changed, return
+
+      console.log("MutationData", mutationData);
+
+      const tabs = document.querySelectorAll(".tabs-container > .tab");
+
+      //Remove all the classes
+      tabs.forEach((tab) => {
+        tab.classList.remove("newTab");
+        tab.classList.remove("moveLeft");
+        tab.classList.remove("moveRight");
+        void (tab as HTMLElement).offsetWidth; //This line is very important, it forces the browser to reflow the element
+      });
+
+      if (mutationData.added) {
+        (mutationData.updated[0] as HTMLElement).classList.add("newTab");
+        for (let i = 1; i < mutationData.updated.length; i++) {
+          (mutationData.updated[i] as HTMLElement).classList.add("moveRight");
+        }
+      } else if (mutationData.removed) {
+        for (let i = 0; i < mutationData.updated.length; i++) {
+          (mutationData.updated[i] as HTMLElement).classList.add("moveLeft");
+        }
+      }
+    });
+
+    //Set interval to add the observer to the tabs-container
+    const addTabsObserver = setInterval(() => {
+      const tabsContainer = document.querySelector(".tabs-container");
+
+      if (!tabsContainer) return; //If the tabs-container does not exist, return
+      clearInterval(addTabsObserver); //Clear the interval
+
+      //Add the observer to the tabs-container element to listen for changes
+      observer.observe(tabsContainer, {
+        childList: true, //Listen to tabs being added or removed
+        attributes: true, //Listen to changes on the tabs
+        attributeOldValue: true, //Get changes on the tabs
+        attributeFilter: ["title"], //Only listen to changes on the title attribute
+        subtree: true, //Listen to the tabs-container children as well
+      });
+    }, 10);
   }
 })();
