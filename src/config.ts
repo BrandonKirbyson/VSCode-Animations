231a34792c7a58a1140f6a700fd44d0034bcbe60
BrandonKirbyson <brandonkirbyson@gmail.com>
2023-05-23 14:44:08 -0700

    Updated to filter out old js config paths of this extension

--- src/config.ts
+++ src/config.ts
@@ -6,20 +6,54 @@ import * as vscode from "vscode";
  */
 export function addToConfig(path: string): Thenable<boolean> {
   const config = vscode.workspace.getConfiguration();
-  const customCssImports = config.get<string[]>("vscode_custom_css.imports"); //Get the current list of imports
-  if (customCssImports && !customCssImports.includes(path)) {
-    //If the list exists and the root CSS file is not already in the list
-    customCssImports.push(path); //Add the root CSS file to the list
-    return config
-      .update(
-        //Update the list of imports
-        "vscode_custom_css.imports",
-        customCssImports,
-        vscode.ConfigurationTarget.Global
-      )
-      .then(() => true); //Returns the promise of the update with a value of true since it actually updated
+  let customImports = config.get<string[]>("vscode_custom_css.imports"); //Get the current list of imports
+
+  if (customImports) customImports = removeOldConfigPaths(path, customImports); //Remove any old paths from the list
+
+  //If the path is added, this will be set to true
+  let pathAdded = false;
+
+  //If the list exists and the root CSS file is not already in the list
+  if (customImports && !customImports.includes(path)) {
+    customImports.push(path); //Add the root CSS file to the list
+    pathAdded = true;
   }
-  return Promise.resolve(false); //Returns a promise that resolves to false
+  return config
+    .update(
+      //Update the list of imports
+      "vscode_custom_css.imports",
+      customImports,
+      vscode.ConfigurationTarget.Global
+    )
+    .then(() => pathAdded); //Update the list of imports and returns if the path was added or not
+}
+
+/**
+ * Removes any old paths from the list of imports in the settings.json file for the Custom CSS and JS Loader extension
+ * @param currentPath The current path to the root js file
+ * @param configPaths The list of paths in the settings.json file for the Custom CSS and JS Loader extension
+ * @returns The list of paths with any old paths removed
+ */
+function removeOldConfigPaths(currentPath: string, configPaths: string[]) {
+  const endOfPath = "/dist/updateHandler.js";
+  const endOfPathDev = "VSCode-Animations/dist/updateHandler.js";
+  let cleanedConfigPaths: string[] = [];
+  for (let i = 0; i < configPaths.length; i++) {
+    const path = configPaths[i];
+    if (path === currentPath) cleanedConfigPaths.push(path); //If the path is the current path, keep it
+
+    if (
+      (path.substring(path.length - endOfPath.length) === endOfPath || //If path ends with /dist/updateHandler.js
+        path.substring(path.length - endOfPathDev.length) === endOfPathDev) && //If path ends with VSCode-Animations/dist/updateHandler.js
+      (path.includes("VSCode-Animations") ||
+        path.includes("brandonkirbyson.vscode-animations")) //If path contains reference to VSCode-Animations
+    )
+      continue;
+
+    cleanedConfigPaths.push(path); //Keep anything that wasn't filtered out
+  }
+  console.log("Config paths", cleanedConfigPaths);
+  return cleanedConfigPaths;
 }
 
 /**
@@ -29,6 +63,7 @@ export function addToConfig(path: string): Thenable<boolean> {
 export function removeFromConfig(path: string): Thenable<void> {
   const config = vscode.workspace.getConfiguration();
   const customCssImports = config.get<string[]>("vscode_custom_css.imports"); //Get the current list of imports
+  if (customCssImports) removeOldConfigPaths(path, customCssImports);
   if (customCssImports && customCssImports.includes(path)) {
     //If the list exists and the root CSS file is in the list
     customCssImports.splice(customCssImports.indexOf(path), 1); //Remove the root CSS file from the list
