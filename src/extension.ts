d845c31c79be3939f779be0d81ccb8c1c46e843a
BrandonKirbyson <brandonkirbyson@gmail.com>
2023-08-08 20:55:40 -0700

    Added auto install toggle and get script path command

--- src/extension.ts
+++ src/extension.ts
@@ -15,73 +15,92 @@ export function activate(context: vscode.ExtensionContext) {
     "/dist/updateHandler.js"
   ).replace(/\\/g, "/"); //Replace all backslashes with forward slashes
 
+  const autoInstall = vscode.workspace
+    .getConfiguration("animations")
+    .get("Auto-Install") as boolean; //Get the auto install setting
+
   initMessenger(); //Initialize the messenger (status bar item)
 
   generateCSS(context); //Generate the css file (just in case)
 
-  //Add the root css file to the custom css imports
-  addToConfig(rootJSPath).then((added) => {
-    //If the root css file was added to the config
-    if (added) {
-      vscode.window
-        .showInformationMessage(
-          "Install VSCode Animations, install will reload window for animations to take effect",
-          "Install",
-          "Cancel"
-        )
-        .then((value) => {
-          //If the user clicked the reload button
-          if (value === "Install") {
-            //Install the custom css extension
-            vscode.commands
-              .executeCommand("extension.installCustomCSS")
-              .then(() => {
-                vscode.commands.executeCommand("workbench.action.reloadWindow"); //Reload the window
-              });
-          }
-        });
-    }
-  });
+  context.subscriptions.push(
+    vscode.commands.registerCommand("VSCode-Animations.getScriptPath", () => {
+      vscode.env.clipboard.writeText(rootJSPath);
+      vscode.window.showInformationMessage(
+        `Animations Script Path Copied to Clipboard! (${rootJSPath})`
+      );
+    })
+  );
 
-  /**
-   * Checks if the Custom CSS and JS Loader extension is installed/enabled and prompts the user to install/enable it if it is not installed/enabled
-   */
-  if (!vscode.extensions.getExtension("be5invis.vscode-custom-css")) {
-    vscode.window
-      .showWarningMessage(
-        "VSCode Animations: The Custom CSS and JS Loader extension is not installed/enabled. Please install or enable it to use this extension.",
-        "Install/Enable"
-      )
-      .then((value) => {
-        if (value === "Install/Enable") {
-          //Open the extension page in the marketplace
-          vscode.commands.executeCommand(
-            "vscode.open",
-            vscode.Uri.parse("vscode:extension/be5invis.vscode-custom-css")
-          );
-          //Add an event listener to the installation of the extension
-          vscode.extensions.onDidChange(() => {
-            //If the extension is installed
-            if (vscode.extensions.getExtension("be5invis.vscode-custom-css")) {
-              vscode.window
-                .showInformationMessage(
-                  "The Custom CSS and JS Loader extension has been installed/enabled so now VSCode Animations can properly work. Reload to see changes.",
-                  "Reload"
-                )
-                .then((value) => {
-                  if (value === "Reload") {
-                    vscode.commands.executeCommand(
-                      "extension.installCustomCSS"
-                    );
-                    vscode.commands.executeCommand(
-                      "workbench.action.reloadWindow"
-                    );
-                  }
+  if (autoInstall) {
+    //Add the root css file to the custom css imports
+    addToConfig(rootJSPath).then((added) => {
+      //If the root css file was added to the config
+      if (added) {
+        vscode.window
+          .showInformationMessage(
+            "Install VSCode Animations, install will reload window for animations to take effect",
+            "Install",
+            "Cancel"
+          )
+          .then((value) => {
+            //If the user clicked the reload button
+            if (value === "Install") {
+              //Install the custom css extension
+              vscode.commands
+                .executeCommand("extension.installCustomCSS")
+                .then(() => {
+                  vscode.commands.executeCommand(
+                    "workbench.action.reloadWindow"
+                  ); //Reload the window
                 });
             }
           });
-        }
-      });
+      }
+    });
+
+    /**
+     * Checks if the Custom CSS and JS Loader extension is installed/enabled and prompts the user to install/enable it if it is not installed/enabled
+     */
+    if (!vscode.extensions.getExtension("be5invis.vscode-custom-css")) {
+      vscode.window
+        .showWarningMessage(
+          "VSCode Animations: The Custom CSS and JS Loader extension is not installed/enabled. Please install or enable it to use this extension.",
+          "Install/Enable"
+        )
+        .then((value) => {
+          if (value === "Install/Enable") {
+            //Open the extension page in the marketplace
+            vscode.commands.executeCommand(
+              "vscode.open",
+              vscode.Uri.parse("vscode:extension/be5invis.vscode-custom-css")
+            );
+            //Add an event listener to the installation of the extension
+            vscode.extensions.onDidChange(() => {
+              //If the extension is installed
+              if (
+                vscode.extensions.getExtension("be5invis.vscode-custom-css")
+              ) {
+                vscode.window
+                  .showInformationMessage(
+                    "The Custom CSS and JS Loader extension has been installed/enabled so now VSCode Animations can properly work. Reload to see changes.",
+                    "Reload"
+                  )
+                  .then((value) => {
+                    if (value === "Reload") {
+                      vscode.commands.executeCommand(
+                        "extension.installCustomCSS"
+                      );
+                      vscode.commands.executeCommand(
+                        "workbench.action.reloadWindow"
+                      );
+                    }
+                  });
+              }
+            });
+          }
+        });
+    }
   }
 
   context.subscriptions.push(
